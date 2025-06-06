9937c4073fa7f28497688f8154647cbb667a2920
ericAleixo <ericaleixo66@gmail.com>
2025-06-06 16:53:34 -0300

    Fix: key

--- config.php
+++ config.php
@@ -1,7 +1,2 @@
 <?php
 
-define('DB_HOST', 'sql10.freesqldatabase.com');
-define('DB_PORT', '3306');
-define('DB_NAME', 'sql10783264');
-define('DB_USER', 'sql10783264');
-define('DB_PASSWORD', 'NtFfSIdUPD'); 
